59ca54cb3262716ce2862d8323c8c22d37f4f96b
i <17203@portalcedi.edu.mx>
2025-02-06 09:09:08 -0600

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ChuckService } from '../joke.service';
+import { CategoriasService } from '../categorias.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  norris: any;
+  categorias: any = [];
+
+  constructor(private ChuckService: ChuckService, private CategoriasService: CategoriasService) {
+  }
+  ngOnInit() {
+    this.ChuckService.getImageOfTheDay().subscribe((data) => {
+      this.norris = data;
+    });
+    this.CategoriasService.getlist().subscribe((data) => { this.categorias = data, console.log(data.results) });
+  }
+
+
+
+
+
+}
---
+++ src/app/joke.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ChuckService } from './joke.service';
+
+describe('ChuckService', () => {
+  let service: ChuckService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ChuckService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
